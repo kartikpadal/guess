7994015defcc38f064b3de2cc0ba699b07d65d5d
kartikpadal <kartikpadal17@gmail.com>
2026-06-12 19:29:15 +0530

    completed the all tabs and their styling

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,11 +1,10 @@
 import { View, Text } from 'react-native'
-import React from 'react'
 import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
 import { styled } from "nativewind";
 
 const SafeAreaView = styled(RNSafeAreaView);
 
-const insights = () => {
+const Insights = () => {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
       <Text>insights</Text>
@@ -13,4 +12,4 @@ const insights = () => {
   )
 }
 
-export default insights;
+export default Insights;
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,15 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import { styled } from "nativewind";
 
-const settings = () => {
+const SafeAreaView = styled(RNSafeAreaView);
+
+const Settings = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text>settings</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
-export default settings;
+export default Settings;
