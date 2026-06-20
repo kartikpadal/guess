9fb66126e0e41d32981c66b793f8027800166e37
kartikpadal <kartikpadal17@gmail.com>
2026-06-20 19:51:14 +0530

    completed profile bar and balance card

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,11 @@
 import "@/global.css"
-import { Text } from "react-native";
+import { Text, View, Image } from "react-native";
 import { Link } from "expo-router";
+import images from "@/constants/images";
+import { HOME_USER , HOME_BALANCE} from "@/constants/data";
+import {icons} from "@/constants/icons";
+import {formatCurrency} from"@/lib/utils";
+import dayjs from 'dayjs';
 import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
 import { styled } from "nativewind";
 
@@ -9,12 +14,29 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-5xl font-sans-extrabold "> Home </Text>
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar"/>
+          <Text className="home-user-name">{HOME_USER.name}</Text>
+        </View>
 
-      <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to onboarding</Link>
-      <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Sign In</Link>
-      <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Go to Sign Up</Link>
+        <Image source={icons.add} className="home-add-icon"/>
+      </View>
 
+      <View className="home-balance-card">
+        <Text className="home-balance-label">Balance</Text>
+
+        <View className="home-balance-row">
+          <Text className="home-balance-amount">
+              {formatCurrency(HOME_BALANCE.amount)}
+          </Text>
+          <Text className="home-balance-date">
+              {dayjs(HOME_BALANCE.nextRenewalDate).format('DD/MM')}
+          </Text>
+        </View>
+      </View>
+      
+      
       
     </SafeAreaView>
   );
--- constants/images.ts
+++ constants/images.ts
@@ -1,3 +1,3 @@
 import splashPattern from '@/assets/images/splash-pattern.png';
-import avatar from '@/assets/imgaes/avatar.png';
+import avatar from '@/assets/images/avatar.png';
 export default {splashPattern, avatar};
